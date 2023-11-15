8b2931fa20cebccc4cd3068430ecf4429c5652f3
BhaskarChandraJoshi <bhaskarjoshe@gmail.com>
2023-11-15 07:20:38 +0530

    Delete product from cart on click

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart=[{
+export let cart=[{
 productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
 quantity:2
 },{
@@ -25,4 +25,16 @@ quantity:selectedQuantity(productId)
 
 function selectedQuantity(productId){
 return(parseInt(document.querySelector(`.js-quantity-selector-${productId}`).value))
+}
+
+
+export function removeFromCart(productId){
+const newCart=[];
+cart.forEach((cartItem)=>{
+if(cartItem.productId!==productId){
+newCart.push(cartItem);
+}
+});
+cart=newCart;
+
 }
