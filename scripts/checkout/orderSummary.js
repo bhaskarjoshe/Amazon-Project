195e8f800ddfdc341e4206dd4bd6adb400b62210
BhaskarChandraJoshi <bhaskarjoshe@gmail.com>
2023-11-22 12:24:37 +0530

    Made delivery unavailable on weekends

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,7 +3,7 @@ updateDeliveryOption} from '../../data/cart.js'
 import {products,getProduct} from '../../data/products.js'
 import formatCurrency from '../utils/money.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
-import {deliveryOptions,getDeliveryOption} from '../../data/deliveryOptions.js'
+import {deliveryOptions,getDeliveryOption,calculateDeliveryDate} from '../../data/deliveryOptions.js'
 import {renderPaymentSummary} from './paymentSummary.js'
 
 export function renderOrderSummary(){
@@ -19,12 +19,7 @@ const deliveryOptionId=cartItem.
 deliveryOptionId;
 
 const deliveryOption=getDeliveryOption(deliveryOptionId);
-
-const today=dayjs();
-const deliveryDate=today.add(deliveryOption.deliveryDays,'days'
-);
-const dateString= deliveryDate.format('dddd , MMMM D');
-
+const dateString=calculateDeliveryDate(deliveryOption);
 
 cartSummaryHTML+=`
 <div class="cart-item-container
@@ -51,7 +46,8 @@ Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">$
 data-product-id="${matchingProduct.id}">
 Update
 </span>
-<input class="quantity-input js-quantity-input-${matchingProduct.id}">
+<input class="quantity-input js-quantity-input-${matchingProduct.id}"
+type="number" min="0" step="1">
 <span class="save-quantity-link link-primary js-save-link"
 data-product-id="${matchingProduct.id}">Save</span>
 <span class="delete-quantity-link link-primary js-delete-link"
@@ -77,12 +73,8 @@ ${deliveryOptionsHTML(matchingProduct,cartItem)}
 function deliveryOptionsHTML(matchingProduct,cartItem){
 let html='';
 deliveryOptions.forEach((deliveryOption)=>{
-const today=dayjs();
-const deliveryDate=today.add(
-deliveryOption.deliveryDays,
-'days'
-);
-const dateString= deliveryDate.format('dddd ,MMMM D');
+const dateString=calculateDeliveryDate(deliveryOption);
+
 const priceString = deliveryOption.priceCents===0?'FREE':`$${formatCurrency(deliveryOption.priceCents)} - `;
 
 
